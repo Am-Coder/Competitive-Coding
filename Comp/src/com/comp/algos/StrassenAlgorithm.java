be0abdee6b823b3208d5f85d08c661da7c207162
Am-Coder <mishraaman2210@gmail.com>
2021-03-15 17:42:32 +0530

    Structuring Repo

--- Comp/src/com/comp/algos/StrassenAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package com.comp.algos;
-		
-public class StrassenAlgorithm {
-	public static void main(String[] args) {
-//		int[][] arr = new int[2][3];
-		
-	}	
-}		
-		
--- Comp/src/com/comp/hearth/HelpKatekar.java
+++
@@ -1,5 +0,0 @@
-package com.comp.hearth;
-
-public class HelpKatekar {
-
-}
